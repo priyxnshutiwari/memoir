e91df301aff4f49335568616299c95f9cbe048e9
priyxnshutiwari <artisanpriyanshu@gmail.com>
2024-08-17 21:16:19 +0530

    third commit

---
+++ src/com/assignment/simpleinterest.java
@@ -0,0 +1,17 @@
+package com.assignment;
+
+import java.util.Scanner;
+
+public class simpleinterest {
+        public static void main(String args[]) {
+            Scanner in = new Scanner(System.in);
+            System.out.print("Enter principal: ");
+            double p = in.nextDouble();
+            System.out.print("Enter rate: ");
+            double r = in.nextDouble();
+            System.out.print("Enter time: ");
+            int t = in.nextInt();
+            double si = p * r * t / 100;
+            System.out.println("Simple Interest: " + si);
+    }
+}
