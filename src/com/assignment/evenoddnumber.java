3febe6b286f285a67ab6fd0a5e45e85e46114ff3
priyxnshutiwari <artisanpriyanshu@gmail.com>
2024-08-17 20:38:15 +0530

    first commit

---
+++ src/com/assignment/evenoddnumber.java
@@ -0,0 +1,18 @@
+package com.assignment;
+
+import java.util.Scanner;
+
+public class evenoddnumber {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("please enter a number : ");
+        int num  = in.nextInt();
+        if ( num % 2 == 0) {
+            System.out.println("even");
+
+        }
+        else{
+            System.out.println("odd");
+        }
+    }
+}
