2be6bc42ed576ca9fa81e282246edaf6a1f13301
priyxnshutiwari <artisanpriyanshu@gmail.com>
2024-08-18 11:17:30 +0530

    did it in one go

---
+++ src/com/assignment/Largestnumber.java
@@ -0,0 +1,22 @@
+package com.assignment;
+
+import java.util.Scanner;
+
+public class Largestnumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter the first and the second number:");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a>b) {
+            System.out.println(a + " is greater than " + b);
+        }
+        else if (a<b)
+        {
+            System.out.println(b + " is greater than " + a);
+        }
+
+    }
+}
