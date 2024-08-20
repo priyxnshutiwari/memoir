124a04c06af3fdac368d95b81658b8f2a052b5f4
priyxnshutiwari <artisanpriyanshu@gmail.com>
2024-08-20 06:46:35 +0530

    did it in one go

---
+++ src/com/condiltionloops/factor.java
@@ -0,0 +1,18 @@
+package com.condiltionloops;
+
+import java.util.Scanner;
+
+public class factor {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("enter a number");
+        int num = in.nextInt();
+
+        for (int i = 2; i < num ; i++) {
+            if (num % i == 0) {
+                System.out.print(i + " ");
+            }
+            }
+            }
+        }
