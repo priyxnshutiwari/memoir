c7afebb3de0f0dad3a742bd21027463bd106f80c
priyxnshutiwari <artisanpriyanshu@gmail.com>
2024-08-21 16:09:30 +0530

    twt

---
+++ src/com/loopsintermediate/vowelconsonant.java
@@ -0,0 +1,28 @@
+package com.loopsintermediate;
+
+import java.util.Scanner;
+
+public class vowelconsonant {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("enter string : ");
+        String input = in.nextLine();
+
+        input = input.toLowerCase();
+
+        int vowels = 0, consonants = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+                vowels++;
+            } else if (ch >= 'a' && ch<= 'z') {
+                consonants++;
+            }
+        }
+        System.out.println("number of vowels : " + vowels);
+        System.out.println("number of vowels : " + consonants);
+    }
+}
