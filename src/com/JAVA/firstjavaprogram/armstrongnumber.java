ce0e69717935d9e69815811cd12fd64646d4bc94
priyxnshutiwari <artisanpriyanshu@gmail.com>
2024-09-25 12:14:49 +0530

    finding armstrong numbers between two numbers

--- src/com/JAVA/firstjavaprogram/armstrongnumber.java
+++ src/com/JAVA/firstjavaprogram/armstrongnumber.java
@@ -6,28 +6,33 @@ public class armstrongnumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        // Input the range a and b
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        findarmstrong(a,b);
-
+        // Call the function to find Armstrong numbers in the given range
+        findArmstrong(a, b);
     }
-        static void findarmstrong(int a, int b)  //this is a method/function
-        {
-            for(int i = a; i<=b; i++)
-            {
-                int num=i;
-                int sum=0;
 
-                while(num>0){
-                    int remainder = num%10;
-                    sum = sum + (remainder*remainder*remainder);
-                    num=num/10;
+    // Function to find Armstrong numbers between 'a' and 'b'
+    static void findArmstrong(int a, int b) {
+        // Loop through the numbers from a to b
+        for (int i = a; i <= b; i++) {
+            int num = i;
+            int sum = 0;
 
-                }
-                if(sum==i){
-                    System.out.println("sum");
-                }
+            // Find the sum of cubes of digits of the number
+            while (num > 0) {
+                int remainder = num % 10;  // Get the last digit
+                sum = sum + (remainder * remainder * remainder);  // Cube the digit and add to sum
+                num = num / 10;  // Remove the last digit
             }
-    }
+
+            // Check if the sum of cubes of digits is equal to the original number
+            if (sum == i) {
+                // Print the Armstrong number
+                System.out.println(i);
+            }
         }
+    }
+}
